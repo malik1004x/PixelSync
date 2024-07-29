eea998b5b5fa0ed2868eb4512da1bc816fd70f86
malik1004x <malikowskirobert@gmail.com>
2024-07-29 22:37:51 +0200

    Initial Commit

---
+++ PixelSync/PixelSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixelSyncApp.swift
+//  PixelSync
+//
+//  Created by Robert Malikowski on 29/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct PixelSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
